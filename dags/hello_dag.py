1fef382073704f3cf4219ce797f5431b100efb51
Byeongjae Jung <jbj616@gmail.com>
2026-04-30 20:29:46 +0900

    Create hello_dag.py

---
+++ dags/hello_dag.py
@@ -0,0 +1,23 @@
+from datetime import datetime
+from airflow.sdk import dag, task  
+
+@dag(
+    schedule="@daily",
+    start_date=datetime(2026, 2, 1),
+    catchup=False,
+    tags=["example"],
+)
+def hello_dag():
+
+    @task
+    def hello():
+        print("FWFWF Hello, world!")
+        return "done"
+
+    @task
+    def bye(msg: str):
+        print(f"fwfwf{msg}, byebye")
+
+    bye(hello())
+
+hello_dag()
